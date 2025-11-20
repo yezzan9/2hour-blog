3a1915acb9fa9418e25f4f3d458c8ee25836ba73
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-11-20 17:02:46 +0900

    feat: 헤더 UI 구현

---
+++ app/_components/Layout/Header.tsx
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-items">
+        <div>logo</div>
+        <div className="menu">
+          <div>Menu</div>
+          <div>Menu</div>
+          <div>Menu</div>
+          <div>Menu</div>
+        </div>
+        <div className="search">
+          <div>Search</div>
+          <input />
+        </div>
+      </div>
+      <div className="line" />
+    </div>
+  );
+};
+
+export default Header;
