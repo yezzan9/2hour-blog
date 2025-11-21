beaedcd0660023edfb3320632ce9a00c1e4807b8
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-11-21 17:03:54 +0900

    chore: 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
 import Header from "./_components/Layout/Header";
 import Footer from "./_components/Layout/Footer";
 import "./styles/main.scss";
+import localFont from "next/font/local";
+
+export const montserrat = localFont({
+  src: "../public/fonts/Montserrat-VariableFont_wght.ttf",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-montserrat",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={montserrat.className}>
       <body>
         <Header />
         <main>{children}</main>
